65b8b7c4860a52c59ba665db91fa23898c8fae41
Cybersecure09 <113461561+Cybersecure09@users.noreply.github.com>
2023-02-02 11:19:52 +0530

    Create gencalgivemy.py

---
+++ gencalgivemy.py
@@ -0,0 +1,6 @@
+import calendar  
+  
+year = int(input ("Please enter the Year: "))   
+month = int(input ("Please enter the month: "))   
+
+Print ("The Calendar of: ", calendar.month(year, month)) 
